3721730cb886c3ff9315f3f6eee8743dabdbb15e
Joey Hiew <joeyhiew@live.com.sg>
2024-02-22 15:11:42 +0800

    feat: initialize ads

--- App.js
+++ App.js
@@ -3,14 +3,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Tabs } from "./components/tab";
 import { StatusBar } from "expo-status-bar";
 import { AppStateProvider } from "./utils/context";
-// import mobileAds from "react-native-google-mobile-ads";
+import mobileAds from "react-native-google-mobile-ads";
 
 export default function App() {
-  // mobileAds()
-  //   .initialize()
-  //   .then((adapterStatuses) => {
-  //     // Initialization complete!
-  //   });
+  mobileAds()
+    .initialize()
+    .then((adapterStatuses) => {
+      // Initialization complete!
+    });
 
   return (
     <AppStateProvider>
